f29a0af09f3775af32254a93f77921bfd73dd3b5
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-04-18 10:22:44 +0530

    job post button submit bug fix

--- client/src/Pages/PostJob.jsx
+++ client/src/Pages/PostJob.jsx
@@ -162,7 +162,7 @@ const onSubmit = async (data) => {};
             <div className='mt-2'>
               <CustomButton
                 type='submit'
-                containerStyles='inline-flex justify-center rounded-md border border-transparent bg-blue-600 px-8 py-2 text-sm font-medium text-white hover:bg-[#1d4fd846] hover:text-[#1d4fd8] focus:outline-none '
+                containerStyles='inline-flex justify-center rounded-md border border-transparent bg-blue px-8 py-2 text-sm font-medium text-white hover:bg-[#1d4fd846] hover:text-[#1d4fd8] focus:outline-none '
                 title='Sumbit'
               />
             </div>
