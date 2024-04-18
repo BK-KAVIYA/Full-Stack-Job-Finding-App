e7164f9c6852cc9eaa07d189f298d4c24365dfa9
Baladurage Kavinda <63805835+BK-KAVIYA@users.noreply.github.com>
2024-04-18 22:39:23 +0530

    Nvigation bar update with dynamically change when log in and not

--- client/src/components/NavBar/navbar.jsx
+++ client/src/components/NavBar/navbar.jsx
@@ -1,11 +1,23 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import Logo from "../../../public/images/Logo.png";
 import "./Navbar.css";
 import { Link, NavLink } from "react-router-dom";
 import { FaBarsStaggered, FaXmark } from "react-icons/fa6";
+import { set } from "react-hook-form";
 
 function Navbar() {
   const [isMenuOpen, setIsMenuOpen] = useState(false);
+  const [isLoggin, setIsLoggin] = useState(false);
+
+  const token = localStorage.getItem("token");
+  
+  useEffect(() => {
+    if (token) {
+      setIsLoggin(true);
+    }
+  }, [token]);
+
+
   const handleMenuToggler = () => {
     setIsMenuOpen(!isMenuOpen);
   };
@@ -35,20 +47,32 @@ function Navbar() {
           ))}
         </ul>
         {/* singup and login button */}
-        <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
-          <Link
-            to="/login"
-            className="py-2 px-5 border rounded hover:bg-gray-300"
-          >
-            Login
-          </Link>
-          <Link
-            to="/signup"
-            className="py-2 px-5 border rounded bg-blue text-white  hover:bg-cyan-950"
-          >
-            Sign Up
-          </Link>
-        </div>
+        {!isLoggin ? (
+          <div className="text-base text-primary font-medium space-x-5 hidden lg:block">
+            <Link
+              to="/login"
+              className="py-2 px-5 border rounded hover:bg-gray-300"
+            >
+              Login
+            </Link>
+            <Link
+              to="/signup"
+              className="py-2 px-5 border rounded bg-blue text-white  hover:bg-cyan-950"
+            >
+              Sign Up
+            </Link>
+          </div>
+        ) : (
+          <div className="flex items-center text-base text-primary font-medium space-x-2">
+            <img
+              src="https://media.licdn.com/dms/image/D5603AQGVruyFtHN25g/profile-displayphoto-shrink_200_200/0/1710589784479?e=1718841600&v=beta&t=dZj7qZVwAqL-OpHzO-_Urh3Lu4gq4ga-hT17RjNGksk"
+              alt="Profile"
+              className="rounded-full h-10 w-10"
+            />
+            <span className="text-lg">Hi</span>
+            <span className="text-lg">Kavinda</span>
+          </div>
+        )}
         <div className="md:hidden block">
           <button onClick={handleMenuToggler}>
             {isMenuOpen ? (
